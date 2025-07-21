d6f430f320c9c6efde636e98805be97de80ce15f
Cody Ryan <codyryan2307@gmail.com>
2025-07-21 19:34:20 +0100

    New postgresql + Neon

---
+++ web/src/components/navbar.js
@@ -0,0 +1,12 @@
+import { link } from '@redwoodjs/router'
+
+const navbar = () => (
+  <nav className="navbar">
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about-me">About Me</Link></li>
+      <li><Link to="/projects">Projects</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+    </ul>
+  </nav>
+)
